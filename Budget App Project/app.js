0a7570abdbded9b42c9cb4a2aa71f8ab2672c158
choudharyRahul22 <32006288+choudharyRahul22@users.noreply.github.com>
2017-10-02 23:48:46 +0530

    Added Budget App

---
+++ Budget App Project/app.js
@@ -0,0 +1,80 @@
+// BUDGET CONTROLLER
+var budgetController = (function(){
+    
+    
+})();
+
+
+// UI CONTROLLER
+var UIController = (function(){
+    
+    var DOMStrings = {
+        
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
+    
+    return {
+        
+        getInput: function(){
+            
+            return{
+                
+                 type: document.querySelector(DOMStrings.inputType).value,
+                 description: document.querySelector(DOMStrings.inputDescription).value,
+                 value: document.querySelector(DOMStrings.inputValue).value
+            };
+            
+        },
+        
+        getDOMStrings: function(){
+            return DOMStrings;
+        }
+    }
+    
+})();
+
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl,UICtrl){
+    
+    var DOM = UICtrl.getDOMStrings();
+    
+    var ctrlAddItem = function(){
+        
+        // 1. Get the filled input data 
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+     
+    };
+    
+    
+    document.querySelector(DOM.inputButton).addEventListener('click',ctrlAddItem);
+    
+    
+    
+    document.addEventListener('keypress',function(event){
+        
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+            
+            
+        }
+        
+    })
+    
+})(budgetController,UIController);
+
+
+
